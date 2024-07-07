c6f9a982d3c0f1727923a84cfbcd09456547971d
Rafael Estrada <ntaticat@gmail.com>
2024-07-07 17:13:02 -0600

    update admin registrar-tema

--- src/app/data/services/temas.service.ts
+++ src/app/data/services/temas.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { ITemaPost } from '../interfaces/comipy.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TemasService {
+  url = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
+
+  postTema(temaInfo: ITemaPost): Observable<{}> {
+    const method = `${this.url}/temas`;
+
+    const request = {
+      ...temaInfo,
+    };
+
+    return this.http.post(method, request);
+  }
 }
--- src/app/modules/admin/registrar-tema-page/registrar-tema-page.component.ts
+++ src/app/modules/admin/registrar-tema-page/registrar-tema-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CursosService } from '../../../data/services/cursos.service';
 import { TemasService } from '../../../data/services/temas.service';
-import { ICurso } from '../../../data/interfaces/comipy.interface';
+import { ICurso, ITemaPost } from '../../../data/interfaces/comipy.interface';
 
 @Component({
   selector: 'app-registrar-tema-page',
@@ -10,17 +10,41 @@ import { ICurso } from '../../../data/interfaces/comipy.interface';
   styleUrl: './registrar-tema-page.component.css',
 })
 export class RegistrarTemaPageComponent implements OnInit {
+  temaForm: FormGroup;
+
   cursosData: ICurso[] = [];
 
   constructor(
     private fb: FormBuilder,
     private cursosService: CursosService,
     private temasService: TemasService
-  ) {}
+  ) {
+    this.temaForm = this.fb.group({
+      curso_id: ['', Validators.required],
+      titulo: ['', [Validators.required, Validators.maxLength(200)]],
+      descripcion: [''],
+    });
+  }
 
   ngOnInit(): void {
     this.cursosService.getCursos().subscribe((cursos) => {
       this.cursosData = cursos;
     });
+  }
+
+  onSubmit() {
+    if (this.temaForm.invalid) {
+      console.log('Formulario inválido');
+      // Aquí puedes manejar el envío del formulario, por ejemplo, enviarlo a un servicio.
+    }
+
+    console.log('Formulario válido', this.temaForm.value);
+
+    const data: ITemaPost = {
+      ...this.temaForm.value,
+      curso: this.temaForm.get('curso_id')?.value,
+    };
+
+    this.temasService.postTema(data).subscribe();
   }
 }
